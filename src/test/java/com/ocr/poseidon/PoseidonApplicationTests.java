74825ebc9093cc198f0d9e07728195e9f2f4c2af
obaocr <for.oba@outlook.fr>
2020-05-05 11:13:38 +0200

    init

---
+++ src/test/java/com/ocr/poseidon/PoseidonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocr.poseidon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
